9f8374cc702ff69c92b39d8904bf310edea85c65
samohamed <mohamed.saher.23@ump.ac.ma>
2025-06-06 00:49:33 +0100

    the client is established too

--- client/main.go
+++ client/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ":999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprint(conn, "hello world")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(status)
+}
